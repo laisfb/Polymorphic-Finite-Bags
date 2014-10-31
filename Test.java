7826a3dd6e39396531a8716842415acf44ee3ecb
Laís Baumgratz <laisfb@users.noreply.github.com>
2014-10-31 11:02:16 -0400

    Test.update2
    More ugly tests, now for Branch

--- Test.java
+++ Test.java
@@ -3,7 +3,7 @@ public class Test {
     public static void testLeaf() {
         Leaf l = new Leaf();
         
-        System.out.println("Color: " + l.color);
+        //System.out.println("Color: " + l.color);
         System.out.println("Seq: " + l.seq());
         System.out.println("hasNext: " + l.hasNext());
         //System.out.println("here: " + l.here());
@@ -14,9 +14,9 @@ public class Test {
         System.out.println("isEmpty: " + l.isEmpty());
         System.out.println("cardinality: " + l.cardinality());
         System.out.println("member: " + l.member(20));
-        
+ 
         System.out.println("add: " + l.add(20));
-        System.out.println("add w/ parent: " + l.add(20, new Branch(10)));
+        //System.out.println("add w/ parent: " + l.add(20, new Branch(10)));
         //System.out.println("remove: " + l.remove(10));
         
         System.out.println("union1: " + l.union(l));
@@ -32,14 +32,59 @@ public class Test {
         System.out.println("subset2: " + l.subset(l.add(10)));
     
     }
+
+    public static void testBranch() {
+        Branch b = new Branch(20);
+        
+        //System.out.println("Color: " + b.color);
+        System.out.println("Seq: " + b.seq());
+        System.out.println("hasNext: " + b.hasNext());
+        System.out.println("here: " + b.here());
+        System.out.println("next: " + b.next());
+        
+        System.out.println("howMany1: " + b.howMany(10));
+    	System.out.println("howMany2: " + b.howMany(20));
+        System.out.println("empty: " + b.empty());
+        System.out.println("isEmpty: " + b.isEmpty());
+        System.out.println("cardinality1: " + b.cardinality());
+        System.out.println("cardinality1: " + b.add(20).cardinality());
+        System.out.println("member1: " + b.member(10));
+        System.out.println("member2: " + b.member(20));
+ 
+        System.out.println("add1: " + b.add(10));
+        System.out.println("add2: " + b.add(20));
+        System.out.println("add3: " + b.add(20).add(10));
+
+        System.out.println("remove1: " + b.remove(10));
+        System.out.println("remove2: " + b.remove(20));
+        System.out.println("remove3: " + b.add(10).remove(10));
+        
+        System.out.println("union1: " + b.union(b));
+        System.out.println("union2: " + b.union(b.add(10)));
+        
+        System.out.println("inter1: " + b.inter(b.add(10)));
+        System.out.println("inter2: " + b.inter(b.add(20)));
+        
+        System.out.println("diff1: " + b.diff(b));
+        System.out.println("diff2: " + b.diff(b.add(10)));
+        System.out.println("diff2: " + (b.add(10)).diff(b));
+
+	    System.out.println("equal1: " + b.equal(b));
+	    System.out.println("equal2: " + b.equal(b.add(10)));
+	    System.out.println("equal3: " + b.equal(b.add(10).remove(10)));
+        
+        System.out.println("\nsubset1: " + b.subset(b));
+        System.out.println("subset2: " + b.subset(b.add(10)));
+        System.out.println("subset2: " + (b.add(10)).subset(b));
+    }
     
     public static void main(String[] args) {
-        Branch b = new Branch(20);
- 
         System.out.println("Let's get this party started!");
         
+        System.out.println("---------- LEAF ----------");
         testLeaf();
-        //testBranch();
+        System.out.println("---------- BRANCH ----------");
+        testBranch();
     }
 
 }
